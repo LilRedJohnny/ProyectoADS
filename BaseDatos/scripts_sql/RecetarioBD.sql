985e95c55e1c6737d37ab5fd6694b7c366a0b6cd
Juan Berumen <juan.berumen.2403@gmail.com>
2026-01-11 18:14:19 -0600

    Actualizacion de diagramas y codigo sql de la base de datos

--- BaseDatos/scripts_sql/RecetarioBD.sql
+++ BaseDatos/scripts_sql/RecetarioBD.sql
@@ -14,20 +14,29 @@ CREATE TABLE usuario (
 );
 
 CREATE TABLE receta (
-  id_receta INT NOT NULL AUTO_INCREMENT,
-  titulo VARCHAR(40) NOT NULL,
+  id_receta INT AUTO_INCREMENT PRIMARY KEY,
+  titulo VARCHAR(40),
   Descripcion TEXT,
   Instrucciones TEXT,
   Categoria VARCHAR(255),
   Tiempo INT,
   Utensilios VARCHAR(255),
-  PRIMARY KEY (id_receta)
+  imagen_url VARCHAR(255)
 );
 
 CREATE TABLE ingrediente (
   id_ingrediente INT NOT NULL AUTO_INCREMENT,
   Nombre VARCHAR(255),
   PRIMARY KEY (id_ingrediente)
+);
+
+CREATE TABLE HistorialReceta (
+  id_historial INT AUTO_INCREMENT,
+  id_usuario INT NOT NULL,
+  id_receta INT NOT NULL,
+  fecha_preparacion DATETIME DEFAULT CURRENT_TIMESTAMP
+  FOREIGN KEY (id_usuario) REFERENCES usuario(id_usuario),
+  FOREIGN KEY (id_receta) REFERENCES receta(id_receta)
 );
 
 CREATE TABLE ingredientereceta (
@@ -51,52 +60,145 @@ CREATE TABLE favorito (
   FOREIGN KEY (id_receta) REFERENCES receta(id_receta)
 );
 
-INSERT INTO receta (titulo, Descripcion, Instrucciones, Categoria, Tiempo, Utensilios) VALUES
-('Crema de aguacate', 'Pele los aguacates, macháquelos y mézclelos con la crema. Añada leche poco a poco. Rectifique sazón y refrigere.', 'Pele los aguacates, macháquelos y mézclelos con la crema. Añada leche poco a poco. Rectifique sazón y refrigere.', 'Sopa', 30, 'Cacerola, licuadora'),
+INSERT INTO receta (titulo, Descripcion, Instrucciones, Categoria, Tiempo, Utensilios, imagen_url) VALUES
+(
+  'Crema de aguacate',
+  'Pele los aguacates, macháquelos y mézclelos con la crema.',
+  'Pele los aguacates, macháquelos y mézclelos con la crema. Añada leche poco a poco. Rectifique sazón y refrigere.',
+  'Entrada',
+  30,
+  'Cacerola, licuadora',
+  'https://www.unileverfoodsolutions.es/dam/global-ufs/mcos/spain/acm/selling-story-calcmenu/crema-aguacate-con-mayonesa-header.jpg'
+),
+(
+  'Espagueti con salsa',
+  'Espagueti con salsa de jitomate y hierbas.',
+  'Prepare la salsa con jitomate y hierbas. Cueza el espagueti y sirva con salsa y queso.',
+  'Entrada',
+  45,
+  'Olla, cacerola',
+  'https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEh12QJKkVP0qmtXT4qaSklChDpqEmCgM0nN6dyt3nPyKJ3zEoosr2VTDOW5RxQgWkP7rUcZSUCXKYccvXUSL0Dwfahm-OR8e1Em8ULBH2nzzNsH9W7Fm8RHD31xb6mi4T6mx2Q5W3YcMExPHsQBVhY8QlxWvJ8rn0VAf79rHE14tjbVqnpyC3yJVnyvyQ/s16000-rw/espaguetis-tomate-vegetales.JPG'
+),
+(
+  'Crema de espinacas',
+  'Crema de espinacas con huevo.',
+  'Cocine espinacas con caldo, agregue crema y queso. Sirva con huevo pochado.',
+  'Entrada',
+  40,
+  'Cacerola',
+  'https://assets.tmecosys.com/image/upload/t_web_rdp_recipe_584x480_1_5x/img/recipe/ras/Assets/8a093085-b447-44cd-8c6d-0215b099994c/Derivates/b1413912-f436-4d12-ab9b-b3b94c82588b.jpg'
+),
+(
+  'Sopa de calabacitas',
+  'Sopa de calabacitas con cilantro.',
+  'Licúe calabacitas con cilantro. Sofría cebolla, agregue licuado y caldo.',
+  'Entrada',
+  20,
+  'Licuadora, cacerola',
+  'https://www.bancodealimentoschicago.org/wp-content/uploads/2022/04/Zucchini-and-Potato-Tomato-Soup.jpg'
+),
+(
+  'Coditos con jamón',
+  'Coditos con jamón y queso.',
+  'Cueza coditos, sofría jamón con mantequilla, mezcle y hornee con queso.',
+  'Entrdaa',
+  35,
+  'Olla, horno',
+  'https://www.lamichoacanameatmarket.com/wp-content/uploads/2018/12/CODITOS26.jpg'
+),
+(
+  'Tacos de requesón con salsa de cacahuate',
+  'Tortillas rellenas de requesón bañadas con salsa cremosa de cacahuate.',
+  'Cortar cebolla, ajo y tomates. Sofreír con chiles y cacahuates. Agregar caldo y crema, licuar y reservar. Mezclar requesón con tomate y cebolla. Calentar tortillas, rellenar y bañar con salsa. Decorar con crema, panela y brotes.',
+  'Plato fuerte',
+  45,
+  'Cuchillos, tabla, ollas, sartenes, licuadora'
+),
+(
+  'Tinga de setas',
+  'Tinga vegetariana de setas en salsa de jitomate y chipotle.',
+  'Sofreír cebolla en aceite, agregar setas y cocinar. Licuar jitomate, chipotle y ajo, colar y verter sobre las setas. Cocinar hasta espesar, rectificar sal y servir en tortillas con nata y queso.',
+  'Plato fuerte',
+  40,
+  'Comal, sartén, licuadora, colador'
+),
+(
+  'Flan de elote',
+  'Flan cremoso de elote con caramelo.',
+  'Preparar caramelo con azúcar y agua y verter en el molde. Licuar leche condensada, evaporada, huevos, queso crema, elote y vainilla. Verter en el molde, cubrir y hornear a baño María a 200°C por 50 minutos. Refrigerar antes de servir.',
+  'Postre',
+  180,
+  'Sartén, licuadora, molde, horno'
+);
 
-('Espagueti con salsa', 'Espagueti con salsa de jitomate y hierbas.', 'Prepare la salsa con jitomate y hierbas. Cueza el espagueti y sirva con salsa y queso.', 'Pasta', 45, 'Olla, cacerola'),
 
-('Crema de espinacas', 'Crema de espinacas con huevo.', 'Cocine espinacas con caldo, agregue crema y queso. Sirva con huevo pochado.', 'Sopa', 40, 'Cacerola'),
-
-('Sopa de calabacitas', 'Sopa de calabacitas con cilantro.', 'Licúe calabacitas con cilantro. Sofría cebolla, agregue licuado y caldo.', 'Sopa', 20, 'Licuadora, cacerola'),
-
-('Coditos con jamón', 'Coditos con jamón y queso.', 'Cueza coditos, sofría jamón con mantequilla, mezcle y hornee con queso.', 'Pasta', 35, 'Olla, horno');
 
 INSERT INTO ingrediente (Nombre) VALUES
-('Aguacate'),
-('Crema'),
-('Leche'),
-('Espagueti'),
-('Jitomate'),
-('Cebolla'),
+('Tomate'),
 ('Ajo'),
-('Queso Chihuahua'),
-('Espinaca'),
-('Huevo'),
-('Calabacita'),
+('Cebolla blanca'),
+('Cebolla morada'),
+('Tomate verde'),
 ('Cilantro'),
-('Coditos'),
-('Jamón'),
-('Mantequilla');
-
+('Maíz'),
+('Queso panela'),
+('Aceite de girasol'),
+('Chile de árbol seco'),
+('Caldo de pollo'),
+('Crema para cocinar'),
+('Requesón'),
+('Cacahuate tostado'),
+('Crema ácida'),
+('Setas'),
+('Nata'),
+('Queso fresco'),
+('Aceite vegetal'),
+('Jitomate pera'),
+('Chile chipotle adobado'),
+('Tortillas de maíz'),
+('Azúcar'),
+('Agua'),
+('Leche condensada'),
+('Queso crema'),
+('Leche evaporada'),
+('Huevo'),
+('Granos de elote'),
+('Extracto de vainilla');
 INSERT INTO ingredientereceta (id_receta, id_ingrediente, Cantidad, Unidad) VALUES
-(1, 1, 4, 'piezas'),
-(1, 2, 1, 'taza'),
-(1, 3, 4, 'tazas'),
+-- Tacos de requesón con salsa de cacahuate
+(6, 1, 6, 'piezas'),
+(6, 2, 4, 'dientes'),
+(6, 3, 2, 'piezas'),
+(6, 4, 1, 'pieza'),
+(6, 5, 8, 'piezas'),
+(6, 6, 1, 'ramo'),
+(6, 7, 2, 'kg'),
+(6, 8, 200, 'gramos'),
+(6, 9, 4, 'cucharadas'),
+(6, 10, 5, 'piezas'),
+(6, 11, 1, 'taza'),
+(6, 12, 2, 'tazas'),
+(6, 13, 2, 'tazas'),
+(6, 14, 1, 'taza'),
+(6, 15, 1, 'taza'),
 
-(2, 4, 200, 'gramos'),
-(2, 5, 6, 'piezas'),
-(2, 6, 1, 'pieza'),
-(2, 7, 1, 'diente'),
-(2, 8, 250, 'gramos'),
+-- Tinga de setas
+(7, 16, 800, 'gramos'),
+(7, 17, 300, 'gramos'),
+(7, 18, 500, 'gramos'),
+(7, 19, 50, 'ml'),
+(7, 20, 5, 'piezas'),
+(7, 21, 3, 'piezas'),
+(7, 3, 2, 'piezas'),
+(7, 2, 2, 'dientes'),
+(7, 22, 20, 'piezas'),
 
-(3, 9, 0.5, 'kg'),
-(3, 10, 4, 'piezas'),
-(3, 2, 4, 'cucharadas'),
-
-(4, 11, 6, 'piezas'),
-(4, 12, 12, 'ramas'),
-
-(5, 13, 200, 'gramos'),
-(5, 14, 250, 'gramos'),
-(5, 15, 0.75, 'barra');
+-- Flan de elote
+(8, 23, 1, 'taza'),
+(8, 24, 2, 'cucharadas'),
+(8, 25, 1, 'lata'),
+(8, 26, 100, 'gramos'),
+(8, 27, 360, 'ml'),
+(8, 28, 5, 'piezas'),
+(8, 29, 1, 'taza'),
+(8, 30, 1, 'cucharada');
